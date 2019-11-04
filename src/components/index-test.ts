dfd6049076b090099976183cb0f0c3866ff14469
Rob Levin <rob@getethos.com>
2019-11-04 10:04:47 -0800

    First failed attempt to create a definitions file for EDS

---
+++ src/components/index-test.ts
@@ -0,0 +1,1 @@
+import * as eds from 'index'
---
+++ src/components/index.d.ts
@@ -0,0 +1,32 @@
+export const Logo: any
+export const Button: any
+export const CloudinaryImage: any
+export const CLOUDINARY_CLOUD_NAME: any
+export const COLORS: any
+export function Layout(props: any): any
+export function RadioButtonGroup(props: any): any
+export const Spacer: any
+export function TextInput(props: any): any
+export function TextAreaInput(props: any): any
+export function TextMaskedInput(props: any): any
+export function PasswordInput(props: any): any
+export function EmailInput(props: any): any
+export const InfoMessage: any
+export function ZipInput(props: any): any
+export function CheckboxInput(props: any): any
+export function Form(props: any): any
+export function BirthdateInput(props: any): any
+export const BirthdateInputValidators: any
+export const UniversalNavbar: any
+export function ButtonSelectGroup(props: any): any
+export function NumberInput(props: any): any
+export const OPTION_BUTTON_STYLES: any
+export const Body: any
+export const Caption: any
+export const Footnote: any
+export const TitleSmall: any
+export const TitleMedium: any
+export const TitleLarge: any
+export const TitleXLarge: any
+export const TitleXXLarge: any
+export const Link: any
